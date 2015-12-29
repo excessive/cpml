1033713b3329d8cc8c84ecd9eb93c9d4b5eff513
Landon Manning <lmanning17@gmail.com>
2015-12-29 05:42:25 -0400

    added DBL_EPSILON

--- modules/constants.lua
+++ modules/constants.lua
@@ -5,6 +5,9 @@ local constants = {}
 -- same as C's FLT_EPSILON
 constants.FLT_EPSILON = 1.19209290e-07
 
+-- same as C's DBL_EPSILON
+constants.DBL_EPSILON = 2.2204460492503131e-16
+
 -- used for quaternion.slerp
 constants.DOT_THRESHOLD = 0.9995
 
