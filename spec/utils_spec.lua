0b0bea16ef5f4a86baede08f6f0a7e63cebc9776
Colby Klein <shakesoda@gmail.com>
2017-03-29 05:32:31 -0700

    add some lerp/decay tests

--- spec/utils_spec.lua
+++ spec/utils_spec.lua
@@ -1,7 +1,27 @@
+local vec3      = require "modules.vec3"
 local utils     = require "modules.utils"
 local constants = require "modules.constants"
 
+local function tolerance(v, t)
+	return math.abs(v - t) < 1e-6
+end
+
 describe("utils:", function()
+	it("interpolates between two numbers", function()
+		assert.is_true(tolerance(utils.lerp(0.5, 0, 1), 0.5))
+	end)
+
+	it("interpolates between two vectors", function()
+		local a = vec3(0, 0, 0)
+		local b = vec3(1, 1, 1)
+		local c = vec3(0.5, 0.5, 0.5)
+		assert.is.equal(utils.lerp(0.5, a, b), c)
+	end)
+
+	it("decays exponentially", function()
+		local v = utils.decay(0.5, 0, 1, 1)
+		assert.is_true(tolerance(v, 0.39346934028737))
+	end)
 end)
 
 --[[
